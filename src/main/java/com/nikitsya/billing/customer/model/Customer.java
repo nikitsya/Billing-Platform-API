c8bfdb29ec33d61d544630a34ea46ba0789cb33e
nikitsya <nikitasmiichyk@gmail.com>
2026-06-06 22:41:00 +0100

    reformat Customer.java

--- src/main/java/com/nikitsya/billing/customer/model/Customer.java
+++ src/main/java/com/nikitsya/billing/customer/model/Customer.java
@@ -1,10 +1,6 @@
 package com.nikitsya.billing.customer.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "customers")
