269af339f3fb642188f1c7a59fbe769e9bbe722e
nikitsya <nikitasmiichyk@gmail.com>
2026-07-05 23:43:45 +0100

    Add payment intent repository

---
+++ src/main/java/com/nikitsya/billing/payment_intent/repository/PaymentIntentRepository.java
@@ -0,0 +1,7 @@
+package com.nikitsya.billing.payment_intent.repository;
+
+import com.nikitsya.billing.payment_intent.model.PaymentIntent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentIntentRepository extends JpaRepository<PaymentIntent, Long> {
+}
